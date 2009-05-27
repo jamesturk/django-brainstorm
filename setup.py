e41dd9e09b677d30b0b697230984a676e14a2b18
James Turk <james.p.turk@gmail.com>
2009-05-27 15:32:10 -0400

    preparation for pypi

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-brainstorm',
+    version="0.1",
+    package_dir={'brainstorm': 'brainstorm'},
+    packages=['brainstorm'],
+    package_data={'brainstorm': ['templates/brainstorm/*.html']},
+    description='Django brainstorming site',
+    author='James Turk',
+    author_email='jturk@sunlightfoundation.com',
+    license='BSD License',
+    url='http://github.com/sunlightlabs/django-brainstorm/',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
